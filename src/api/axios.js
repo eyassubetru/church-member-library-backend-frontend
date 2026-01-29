e5aea723f0a3ae09215f61b59298c29742fc958d
Eyassu Betru <eyassubetru0@gmaol.com>
2026-01-29 23:42:40 +0300

    axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,8 @@ import { useAuthStore } from "../store/authStore";
 
 const api = axios.create({
   // baseURL: "http://localhost:5000/api",
-  baseURL: import.meta.env.VITE_API_URL,
+  // baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://church-member-library-backend.onrender.com/api",
   withCredentials: true // IMPORTANT for cookies
 });
 
